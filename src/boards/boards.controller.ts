77891b67ca63489ff6b7c1b007e5fd3bbcc4ac20
nyaongnyaooong <luckyyou123@naver.com>
2023-07-24 21:07:41 +0900

    firstCommit

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) { }
+
+  @Get('/')
+  getAllBoard() {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body() createBoardDto: CreateBoardDto
+  ) {
+    return this.boardsService.createBoard(createBoardDto)
+  }
+
+}
